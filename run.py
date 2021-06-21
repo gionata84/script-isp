c7855484d1b1c4375682d6625f6755b676ef315e
Gionata Coccia <gionata.coccia@spindox.it>
2021-06-21 16:47:09 +0200

    new2

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import re
 
 folder_common = 'tmp_common'
 
-def main(argv):
 
+def main(argv):
     os.makedirs(folder_common)
     master = git.Repo.clone_from('https://github.com/gionata84/demo-common.git', folder_common, branch='main')
     master.close()
@@ -17,47 +17,36 @@ def main(argv):
 def execute(filename):
     target_folder = 'target/' + os.path.splitext(filename)[0]
     os.makedirs(target_folder)
+    values = build_phs(filename)
     for filename in os.listdir(folder_common):
         if filename.endswith(".yml"):
             with open(folder_common + '/' + filename, 'r') as instream:
                 content = instream.read()
-                content = re.sub("\${(.*?)}", replace_var, content)
+                content = re.sub("\${(.*?)}", replace_var(values), content)
                 outF = open(target_folder + "/" + filename, "w")
                 outF.write(content)
                 outF.close()
 
 
+def replace_var(values):
+    def lookup(match):
+        key = match.group(1)
+        return values.get(key, f'<{key} not found>')
 
-def templating_target(env):
-    folder_common = 'tmp_common_' + env
-    folder_out = 'tmp_out' + env
-    os.makedirs(folder_common)
-    os.makedirs(folder_out)
-    master = git.Repo.clone_from('https://github.com/gionata84/demo-common.git', folder_common, branch='main')
-    master.close()
-    for filename in os.listdir(folder_common):
-        if filename.endswith(".yml"):
-            with open(folder_common + '/' + filename, 'r') as instream:
-                content = instream.read()
-                content = re.sub("\${(.*?)}", replace_var, content)
-                outF = open(folder_out + "/" + filename, "w")
-                outF.write(content)
-                outF.close()
+    return lookup
 
 
-def replace_var(match):
-    var = match.group(1)
-    values = create_values()
-    if var in values:
-        return values[var]
-
-
-def create_values():
+def build_phs(config):
     d = {}
+    with open(folder_common + '/configurations/' + config) as f:
+        for line in f:
+            (key, val) = line.split(':')
+            d[(key.strip())] = val.strip()
+
     with open('inputs/input_1') as f:
         for line in f:
             (key, val) = line.split()
-            d[(key)] = val
+            d[(key.strip())] = val.strip()
     return d
 
 
