e5e1ae22da6b8f38c4320aaba19b57adb0e99046
Gionata Coccia <gionata.coccia@spindox.it>
2021-06-21 16:21:50 +0200

    new1

--- run.py
+++ run.py
@@ -3,22 +3,41 @@ import sys
 import os
 import re
 
+folder_common = 'tmp_common'
 
 def main(argv):
-    templating_target('dev')
-    templating_target('prod')
+
+    os.makedirs(folder_common)
+    master = git.Repo.clone_from('https://github.com/gionata84/demo-common.git', folder_common, branch='main')
+    master.close()
+    for filename in os.listdir(folder_common + '/configurations'):
+        execute(filename)
+
+
+def execute(filename):
+    target_folder = 'target/' + os.path.splitext(filename)[0]
+    os.makedirs(target_folder)
+    for filename in os.listdir(folder_common):
+        if filename.endswith(".yml"):
+            with open(folder_common + '/' + filename, 'r') as instream:
+                content = instream.read()
+                content = re.sub("\${(.*?)}", replace_var, content)
+                outF = open(target_folder + "/" + filename, "w")
+                outF.write(content)
+                outF.close()
+
 
 
 def templating_target(env):
-    folder_origin = 'tmp_common_' + env
+    folder_common = 'tmp_common_' + env
     folder_out = 'tmp_out' + env
-    os.makedirs(folder_origin)
+    os.makedirs(folder_common)
     os.makedirs(folder_out)
-    master = git.Repo.clone_from('https://github.com/gionata84/demo-common.git', folder_origin, branch=env)
+    master = git.Repo.clone_from('https://github.com/gionata84/demo-common.git', folder_common, branch='main')
     master.close()
-    for filename in os.listdir(folder_origin):
+    for filename in os.listdir(folder_common):
         if filename.endswith(".yml"):
-            with open(folder_origin + '/' + filename, 'r') as instream:
+            with open(folder_common + '/' + filename, 'r') as instream:
                 content = instream.read()
                 content = re.sub("\${(.*?)}", replace_var, content)
                 outF = open(folder_out + "/" + filename, "w")
