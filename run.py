35c30243d2d710dfb3df77ed0af4d8c5da9f9409
Gionata Coccia <gionata.coccia@spindox.it>
2021-06-22 13:22:57 +0200

    push foo master

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import shutil
+
 import git
 import sys
 import os
@@ -12,6 +14,24 @@ def main(argv):
     master.close()
     for filename in os.listdir(folder_common + '/configurations'):
         execute(filename)
+
+    os.makedirs('target_repo/dev')
+    repo = git.Repo.clone_from('https://gionata84:Arancia0!@github.com/gionata84/foo.git', 'target_repo/dev')
+    move_files('target/dev', 'target_repo/dev')
+    repo.git.add(all=True)
+    repo.index.commit('first')
+    origin = repo.remote(name='origin')
+    origin.push()
+
+
+def move_files(source_dir, target_dir):
+    touple = ['.yml', '.yaml']
+    file_names = os.listdir(source_dir)
+
+    for file_name in file_names:
+        name, extension = os.path.splitext(file_name)
+        if extension in touple:
+            shutil.move(os.path.join(source_dir, file_name), target_dir)
 
 
 def execute(config_file):
